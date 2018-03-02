93d9e2c0d4f86081bb91b8ac4da476912f925e34
David R. MacIver <david@drmaciver.com>
2018-03-02 08:20:12 +0000

    Script for automating these lists of cites I'm writing

--- scripts/autorebuild.sh
+++ scripts/autorebuild.sh
@@ -5,5 +5,9 @@ if ! rubber -d notes.tex ; then
     echo -ne '\007'
 fi
 
+if ! python scripts/reflist.py ; then
+    echo -ne '\007'
+fi
+
 inotifywait notes.tex references.bib chapters chapters/*.tex $0
 exec $0
---
+++ scripts/reflist.py
@@ -0,0 +1,17 @@
+import bibtexparser
+import re
+
+if __name__ == '__main__':
+    with open('references.bib') as bibtex:
+        db = bibtexparser.load(bibtex)
+
+    with open('reflist.tex', 'w') as o:
+        o.write('\\begin{itemize}\n')
+        for entry in db.entries:
+            if entry['ENTRYTYPE'] == 'proceedings':
+                continue
+            title = re.sub(r" +", " ", entry['title'].replace("\n", " "))
+            o.write('\\item %s\\cite{%s}\n' % (
+                title, entry['ID']
+            ))
+        o.write('\\end{itemize}\n')
